af26d5bb1f3128e82712b0ead9a7be1968170ca4
Abdullah <abdullaharif893@gmail.com>
2025-12-15 00:38:34 +0500

    update

--- book/docusaurus.config.js
+++ book/docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https:/AbdullahArif17/.github.io',
+  url: 'https://AbdullahArif17.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/Humanoid_And_Robotis_Book_Hackathon/',
